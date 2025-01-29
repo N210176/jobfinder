59a2a4b5e16e5f0312a520c04fd0a81614f9dc6b
umesh <umeshchandrareddy851@gmail.com>
2025-01-29 19:47:07 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Services from './components/Services/Services';
+import PostJob from './pages/PostJob/PostJob';
+import FindWorkers from './pages/FindWorkers/FindWorkers';
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+import NotFound from './pages/NotFound/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Hero />
+                <Services />
+              </>
+            } />
+            <Route path="/post-job" element={<PostJob />} />
+            <Route path="/find-workers" element={<FindWorkers />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
