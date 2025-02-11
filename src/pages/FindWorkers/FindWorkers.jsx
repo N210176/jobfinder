9a6a51991f6bbd1f3a6c30bb0210a7a1b4f79e6c
Himasree9492 <n210789@rguktn.ac.in>
2025-02-11 11:26:33 +0530

    Update FindWorkers.jsx

--- src/pages/FindWorkers/FindWorkers.jsx
+++ src/pages/FindWorkers/FindWorkers.jsx
@@ -17,3 +17,14 @@ import {
 import { useLocation, useNavigate } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import './FindWorkers.css';
+
+// Import images
+import ahalyaImg from './img/Ahalya.JPG';
+import annieImg from './img/Annie Harshini.JPG';
+import kethamImg from './img/Ketham Babu.JPG';
+import manojImg from './img/Manoj Kumar.JPG';
+import rishikImg from './img/Rishik.JPG';
+import sagarImg from './img/Sagar.JPG';
+import sunilImg from './img/Sunil Kumar.JPG';
+import venkateshImg from './img/Venkatesh.JPG';
+import vijayaImg from './img/Vijaya Lakshmi.JPG';
